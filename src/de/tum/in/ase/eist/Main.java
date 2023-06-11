58d4e6c80e6c09efa9efb152b8690711ee5028ad
Fritz Rehde <fritz.rehde@tum.de>
2023-06-11 16:48:16 +0200

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/eist/Main.java
@@ -0,0 +1,14 @@
+package de.tum.in.ase.eist;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        detectDeadlock(new SwimmingPool());
+    }
+
+    public static void detectDeadlock(SwimmingPool swimmingPool) {
+        // TODO 2
+    }
+}
