991aa5fae2ad6eb4c8e24337523d228a2fc901eb
moritzslz <schultzmoritz@gmail.com>
2023-06-29 20:23:03 +0200

    deadlock

--- src/de/tum/in/ase/eist/Swimmer.java
+++ src/de/tum/in/ase/eist/Swimmer.java
@@ -14,6 +14,6 @@ public class Swimmer {
     }
 
     public void goToSwimmingPool(SwimmingPool swimmingPool, SwimmingPoolActionOrder order) {
-        swimmingPool.handleEntryRequestDeadlockFree(this, order);
+        swimmingPool.handleEntryRequest(this, order);
     }
 }
